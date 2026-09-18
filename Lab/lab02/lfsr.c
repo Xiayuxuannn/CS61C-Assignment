0c1306db9524996e97fff3c400c85a0f74be7d2f
Editer <xiayuxuan259@gmail.com>
2026-09-18 13:34:23 +0800

    lab02:completed

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -5,6 +5,7 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t feedback = (*reg & 0x1) ^ ((*reg >> 2) & 0x1) ^ ((*reg >> 3) & 0x1) ^ ((*reg >> 5) & 0x1);
+    *reg = *reg >>1 | (feedback << 15);
 }
 
