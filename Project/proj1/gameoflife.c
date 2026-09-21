1a85adf3ffdd657c7f65fd52ea34ad50b776de62
Editer <xiayuxuan259@gmail.com>
2026-09-21 15:28:27 +0800

    proj1:down

--- Project/proj1/gameoflife.c
+++ Project/proj1/gameoflife.c
@@ -22,14 +22,55 @@
 //and the left column as adjacent to the right column.
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
-	//YOUR CODE HERE
+	int rows = (int)image->rows;
+	int cols = (int)image->cols;
+	Color *color = malloc(sizeof(Color));
+	uint8_t newR = 0, newG = 0, newB = 0;
+
+	for (int b = 0; b < 8; b++) {                  // 每一位独立处理
+		int aliveR = (image->image[row][col].R >> b) & 1;
+		int aliveG = (image->image[row][col].G >> b) & 1;
+		int aliveB = (image->image[row][col].B >> b) & 1;
+		int kR = 0, kG = 0, kB = 0;                // 活邻居数
+		for (int dr = -1; dr <= 1; dr++) {
+			for (int dc = -1; dc <= 1; dc++) {
+				if (dr == 0 && dc == 0) continue;
+				int nr = (row + dr + rows) % rows;
+				int nc = (col + dc + cols) % cols;
+				kR += (image->image[nr][nc].R >> b) & 1;
+				kG += (image->image[nr][nc].G >> b) & 1;
+				kB += (image->image[nr][nc].B >> b) & 1;
+			}
+		}
+
+		newR |= ((rule >> (aliveR ? 9 + kR : kR)) & 1) << b;
+		newG |= ((rule >> (aliveG ? 9 + kG : kG)) & 1) << b;
+		newB |= ((rule >> (aliveB ? 9 + kB : kB)) & 1) << b;
+
+		color->R = newR;
+		color->G = newG;
+		color->B = newB;
+	}
+	return color;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
 //You should be able to copy most of this from steganography.c
 Image *life(Image *image, uint32_t rule)
 {
-	//YOUR CODE HERE
+	Image *i1 = malloc(sizeof(Image));
+	i1->rows = image->rows;
+	i1->cols = image->cols;
+	i1->image = malloc(i1->rows * sizeof(Color*));
+	for (uint32_t i = 0; i < i1->rows; i++){
+		i1->image[i] = malloc(i1->cols * sizeof(Color));
+		for (uint32_t j = 0; j < i1->cols; j++){
+			Color *c = evaluateOneCell(image, i, j, rule);
+			i1->image[i][j] = *c;
+			free(c);
+		}
+	}
+	return i1;
 }
 
 /*
@@ -49,5 +90,20 @@ You may find it useful to copy the code from steganography.c, to start.
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	if (argc != 3) {
+		printf("usage: ./gameOfLife filename rule\n");
+		printf("filename is an ASCII PPM file (type P3) with maximum value 255.\n");
+		printf("rule is a hex number beginning with 0x; Life is 0x1808.\n");
+		return -1;
+	}
+	Image *Flife = readData(argv[1]);
+	if (Flife == NULL) {
+		return -1;
+	}
+	uint32_t rule = (uint32_t)strtol(argv[2], NULL, 16);
+	Image *Nxtlife = life(Flife, rule);
+	writeData(Nxtlife);
+	freeImage(Flife);
+	freeImage(Nxtlife);
+	return 0;
 }
--- Project/proj1/steganography.c
+++ Project/proj1/steganography.c
@@ -21,13 +21,36 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+	Color *color = malloc(sizeof(Color));
+	if ((image->image[row][col].B & 0x1) == 1){
+		color->G = 255;
+		color->B = 255;
+		color->R = 255;
+	}
+	else{
+		color->G = 0;
+		color->B = 0;
+		color->R = 0;	
+	}
+	return color;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+	Image *i1 = malloc(sizeof(Image));
+	i1->rows = image->rows;
+	i1->cols = image->cols;
+	i1->image = malloc(i1->rows * sizeof(Color*));
+	for (uint32_t i = 0; i < i1->rows; i++){
+		i1->image[i] = malloc(i1->cols * sizeof(Color));
+		for (uint32_t j = 0; j < i1->cols; j++){
+			Color *c = evaluateOnePixel(image, i, j);
+			i1->image[i][j] = *c;
+			free(c);
+		}
+	}
+	return i1;
 }
 
 /*
@@ -45,5 +68,17 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	if (argc != 2) {
+		printf("usage: %s <filename>\n", argv[0]);
+		return -1;
+	}
+	Image *Simage = readData(argv[1]);
+	if (Simage == NULL) {
+		return -1;
+	}
+	Image *Xsimage = steganography(Simage);
+	writeData(Xsimage);
+	freeImage(Simage);
+	freeImage(Xsimage);
+	return 0;
 }
